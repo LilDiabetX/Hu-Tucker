fde0d0034c5b0fae5e1182ea6b91805d4d84ad77
Benjamin Decker <benjamin.t.decker@gmail.com>
2025-03-23 21:22:35 +0100

    benchmark structure

--- benchmark.py
+++ benchmark.py
@@ -9,6 +9,7 @@ path = os.getcwd()
 files_100 = [f for f in os.listdir(path + "/corpus_tests/100_char")]
 files_500 = [f for f in os.listdir(path + "/corpus_tests/500_char")]
 files_1000 = [f for f in os.listdir(path + "/corpus_tests/1000_char")]
+<<<<<<< HEAD
 
 def bench_hu_tucker():
     print("Hu-Tucker benchmark start")
@@ -16,6 +17,15 @@ def bench_hu_tucker():
     times_500 = np.zeros(len(files_500))
     times_1000 = np.zeros(len(files_1000))
     output = open(path + "/output/result_hu_tucker_benchmark.csv", 'w')
+=======
+files_2000 = [f for f in os.listdir(path + "/corpus_tests/2000_char")]
+
+def bench_hu_tucker():
+    times_100 = np.zeros(len(files_100))
+    times_500 = np.zeros(len(files_500))
+    times_1000 = np.zeros(len(files_1000))
+    times_2000 = np.zeros(len(files_2000))
+>>>>>>> b4e783d (benchmark structure)
 
     print("Processing 100 characters files...")
     for i, f in enumerate(files_100):
@@ -34,10 +44,15 @@ def bench_hu_tucker():
         # Phase 3
         hu_tucker_tree = ht.recombination(leaf_levels, debug=False)
         t_end = time.time()
+<<<<<<< HEAD
         output.write(str(t_end - t_start) + ';')
         times_100[i] = t_end - t_start
     print(str(len(files_100)) + " 100 characters files processed in " + str(np.sum(times_100)) + "s")
     output.write('\n')
+=======
+        times_100[i] = t_end - t_start
+    print(str(len(files_100)) + " 100 characters files processed in " + str(np.sum(times_100)) + "s")
+>>>>>>> b4e783d (benchmark structure)
 
     print("Processing 500 characters files...")
     for i, f in enumerate(files_500):
@@ -56,10 +71,15 @@ def bench_hu_tucker():
         # Phase 3
         hu_tucker_tree = ht.recombination(leaf_levels, debug=False)
         t_end = time.time()
+<<<<<<< HEAD
         output.write(str(t_end - t_start) + ';')
         times_500[i] = t_end - t_start
     print(str(len(files_500)) + " 500 characters files processed in " + str(np.sum(times_500)) + "s")
     output.write('\n')
+=======
+        times_500[i] = t_end - t_start
+    print(str(len(files_500)) + " 500 characters files processed in " + str(np.sum(times_500)) + "s")
+>>>>>>> b4e783d (benchmark structure)
 
     print("Processing 1000 characters files...")
     for i, f in enumerate(files_1000):
@@ -78,6 +98,7 @@ def bench_hu_tucker():
         # Phase 3
         hu_tucker_tree = ht.recombination(leaf_levels, debug=False)
         t_end = time.time()
+<<<<<<< HEAD
         output.write(str(t_end - t_start) + ';')
         times_1000[i] = t_end - t_start
     print(str(len(files_1000)) + " 1000 characters files processed in " + str(np.sum(times_1000)) + "s")
@@ -89,16 +110,53 @@ def bench_hu_tucker():
     labels = ['100 char', '500 char', '1000 char']
     fig, axes = plt.subplots(1 + len(times), 1, figsize=(8, 12))
     axes[0].bar(labels, means, color=['blue', 'red', 'green'])
+=======
+        times_1000[i] = t_end - t_start
+    print(str(len(files_1000)) + " 1000 characters files processed in " + str(np.sum(times_1000)) + "s")
+
+    print("Processing 2000 characters files...")
+    for i, f in enumerate(files_2000):
+        file = open("corpus_tests/2000_char/" + f, mode='r', encoding="utf-8")
+        text = file.read()
+        occs = ht.occurences(text)
+        initial_seq = ht.build_initial_seq(occs)
+        # Fin du Set-up
+        t_start = time.time()
+        # Phase 1
+        comb_tree = ht.combination(initial_seq, debug=False)
+
+        # Phase 2
+        leaf_levels = ht.level_assignment(comb_tree, initial_seq)
+
+        # Phase 3
+        hu_tucker_tree = ht.recombination(leaf_levels, debug=False)
+        t_end = time.time()
+        times_2000[i] = t_end - t_start
+    print(str(len(files_2000)) + " 2000 characters files processed in " + str(np.sum(times_2000)) + "s")
+
+    times = [times_100, times_500, times_1000, times_2000]
+    means = [np.mean(t) for t in times]
+    labels = ['100 char', '500 char', '1000 char', '2000 char']
+    fig, axes = plt.subplots(1 + len(times), 1, figsize=(8, 12))
+    axes[0].bar(labels, means, color=['blue', 'red', 'green', 'purple'])
+>>>>>>> b4e783d (benchmark structure)
     axes[0].set_xlabel('nombre de caractères différents')
     axes[0].set_ylabel('Temps moyen')
     axes[0].set_title('Temps moyen par nombre de caractères différents')
 
     for i, tableau in enumerate(times):
+<<<<<<< HEAD
         axes[i + 1].plot(tableau, marker='o', linestyle='', color='black')
         axes[i + 1].set_title(labels[i])
         axes[i + 1].set_ylabel('Temps')
         axes[i + 1].set_xlabel('Fichier')
         axes[i + 1].set_ylim(bottom=0)
+=======
+        axes[i + 1].plot(tableau, marker='o', linestyle='-', color='black')
+        axes[i + 1].set_title(labels[i])
+        axes[i + 1].set_ylabel('Temps')
+        axes[i + 1].set_xlabel('Fichier')
+>>>>>>> b4e783d (benchmark structure)
     plt.tight_layout()
     plt.show()
 
@@ -106,16 +164,24 @@ def bench_hu_tucker():
 
 
 def bench_huffman():
+<<<<<<< HEAD
     print("Huffman benchmark start")
     times_100 = np.zeros(len(files_100))
     times_500 = np.zeros(len(files_500))
     times_1000 = np.zeros(len(files_1000))
     output = open(path + "/output/result_huffman_benchmark.csv", 'w')
+=======
+    times_100 = np.zeros(len(files_100))
+    times_500 = np.zeros(len(files_500))
+    times_1000 = np.zeros(len(files_1000))
+    times_2000 = np.zeros(len(files_2000))
+>>>>>>> b4e783d (benchmark structure)
 
     print("Processing 100 characters files...")
     for i, f in enumerate(files_100):
         file = open("corpus_tests/100_char/" + f, mode='r', encoding="utf-8")
         text = file.read()
+<<<<<<< HEAD
         occs_phrase = hm.occurences(text)
         leafs = hm.build_initial_seq(occs_phrase)
         t_start = time.time()
@@ -126,11 +192,22 @@ def bench_huffman():
         times_100[i] = t_end - t_start
     print(str(len(files_100)) + " 100 characters files processed in " + str(np.sum(times_100)) + "s")
     output.write('\n')
+=======
+        t_start = time.time()
+        occs_phrase = hm.occurences(text)
+        leafs = hm.build_initial_seq(occs_phrase)
+        tree = hm.build_huffman_tree(leafs)
+        huff_code = hm.huffman_code(tree)
+        t_end = time.time()
+        times_100[i] = t_end - t_start
+    print(str(len(files_100)) + " 100 characters files processed in " + str(np.sum(times_100)) + "s")
+>>>>>>> b4e783d (benchmark structure)
 
     print("Processing 500 characters files...")
     for i, f in enumerate(files_500):
         file = open("corpus_tests/500_char/" + f, mode='r', encoding="utf-8")
         text = file.read()
+<<<<<<< HEAD
         occs_phrase = hm.occurences(text)
         leafs = hm.build_initial_seq(occs_phrase)
         t_start = time.time()
@@ -141,11 +218,22 @@ def bench_huffman():
         times_500[i] = t_end - t_start
     print(str(len(files_500)) + " 500 characters files processed in " + str(np.sum(times_500)) + "s")
     output.write('\n')
+=======
+        t_start = time.time()
+        occs_phrase = hm.occurences(text)
+        leafs = hm.build_initial_seq(occs_phrase)
+        tree = hm.build_huffman_tree(leafs)
+        huff_code = hm.huffman_code(tree)
+        t_end = time.time()
+        times_500[i] = t_end - t_start
+    print(str(len(files_500)) + " 500 characters files processed in " + str(np.sum(times_500)) + "s")
+>>>>>>> b4e783d (benchmark structure)
 
     print("Processing 1000 characters files...")
     for i, f in enumerate(files_1000):
         file = open("corpus_tests/1000_char/" + f, mode='r', encoding="utf-8")
         text = file.read()
+<<<<<<< HEAD
         occs_phrase = hm.occurences(text)
         leafs = hm.build_initial_seq(occs_phrase)
         t_start = time.time()
@@ -163,19 +251,58 @@ def bench_huffman():
     labels = ['100 char', '500 char', '1000 char']
     fig, axes = plt.subplots(1 + len(times), 1, figsize=(8, 12))
     axes[0].bar(labels, means, color=['blue', 'red', 'green'])
+=======
+        t_start = time.time()
+        occs_phrase = hm.occurences(text)
+        leafs = hm.build_initial_seq(occs_phrase)
+        tree = hm.build_huffman_tree(leafs)
+        huff_code = hm.huffman_code(tree)
+        t_end = time.time()
+        times_1000[i] = t_end - t_start
+    print(str(len(files_1000)) + " 1000 characters files processed in " + str(np.sum(times_1000)) + "s")
+
+    print("Processing 2000 characters files...")
+    for i, f in enumerate(files_2000):
+        file = open("corpus_tests/2000_char/" + f, mode='r', encoding="utf-8")
+        text = file.read()
+        t_start = time.time()
+        occs_phrase = hm.occurences(text)
+        leafs = hm.build_initial_seq(occs_phrase)
+        tree = hm.build_huffman_tree(leafs)
+        huff_code = hm.huffman_code(tree)
+        t_end = time.time()
+        times_2000[i] = t_end - t_start
+    print(str(len(files_2000)) + " 2000 characters files processed in " + str(np.sum(times_2000)) + "s")
+
+    times = [times_100, times_500, times_1000, times_2000]
+    means = [np.mean(t) for t in times]
+    labels = ['100 char', '500 char', '1000 char', '2000 char']
+    fig, axes = plt.subplots(1 + len(times), 1, figsize=(8, 12))
+    axes[0].bar(labels, means, color=['blue', 'red', 'green', 'purple'])
+>>>>>>> b4e783d (benchmark structure)
     axes[0].set_xlabel('nombre de caractères différents')
     axes[0].set_ylabel('Temps moyen')
     axes[0].set_title('Temps moyen par nombre de caractères différents')
 
     for i, tableau in enumerate(times):
+<<<<<<< HEAD
         axes[i + 1].plot(tableau, marker='o', linestyle='', color='black')
         axes[i + 1].set_title(labels[i])
         axes[i + 1].set_ylabel('Temps')
         axes[i + 1].set_xlabel('Fichier')
         axes[i + 1].set_ylim(bottom=0)
+=======
+        axes[i + 1].plot(tableau, marker='o', linestyle='-', color='black')
+        axes[i + 1].set_title(labels[i])
+        axes[i + 1].set_ylabel('Temps')
+        axes[i + 1].set_xlabel('Fichier')
+>>>>>>> b4e783d (benchmark structure)
     plt.tight_layout()
     plt.show()
     
         
 bench_hu_tucker()
+<<<<<<< HEAD
 bench_huffman()
+=======
+>>>>>>> b4e783d (benchmark structure)
