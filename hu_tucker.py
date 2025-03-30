64e2eb2044dcc53f93c5ad86a027078795197643
GUETTEVILLE nathan <nathan.guetteville@etu.u-paris.fr>
2025-03-31 00:43:48 +0200

    implemented optimal phase 3

--- hu_tucker.py
+++ hu_tucker.py
@@ -2,6 +2,8 @@ import sys
 from tree_struct import Leaf, Node, Tree
 from utils import occurences, build_initial_seq
 from display_tree import plot_tree
+from collections import deque
+
 
 def leafs_between(i, j, nodes):
     for node in nodes[i+1:j]:
@@ -35,7 +37,7 @@ def combination(initial_seq, debug=False):
         nodes[i] = new_node
         nodes.pop(j)
         if debug:
-            plot_tree(Tree(nodes[i]), outputname="wip_phase_1_tree_"+str(k))
+            plot_tree(Tree(nodes[i]), outputname="wip_phase_1_tree_{k}")
             k += 1
     return Tree(nodes[0])
 
@@ -78,7 +80,61 @@ def recombination(leaf_levels, debug=False):
         levels[i] = (new_node, l_i - 1)
         levels.pop(j)
         if debug:
-            plot_tree(Tree(new_node), outputname="wip_phase_3_tree_"+str(k))
+            plot_tree(Tree(new_node), outputname="wip_phase_3_tree_{k}")
             k += 1
     assert(levels[0][1]==0)
     return Tree(levels[0][0])
+
+def recombination_stack(leaf_levels, debug=False):
+    stack = deque() # LIFO - use pop()
+    queue = deque() # FIFO - use popleft()
+    for leaf in leaf_levels:
+        queue.appendleft(leaf)
+    
+    k = 0
+    while queue or len(stack)!=1:
+        if len(stack) < 2 or stack[-1][1] != stack[-2][1]:
+            elt = queue.popleft()
+            stack.append(elt)
+        else:
+            q_1, l_1 = stack.pop()
+            q_2, _ = stack.pop()
+            new_elt = Node(q_1.weight+q_2.weight, q_1, q_2)
+            stack.append((new_elt, l_1-1))
+            if debug:
+                plot_tree(Tree(new_elt), outputname=f"wip_phase_3_opt_tree_{k}")
+                k += 1
+    print(len(stack))
+    assert(len(stack) == 1)
+    assert(stack[0][1] == 0)
+    return Tree(stack[0][0])
+
+
+
+def main(debug):
+    #phrase = "je mange des saucisses seches venant d'estonie"
+    phrase = "aaaaazzeeeeeeerrtyuiiooooppppp" # Même configuration que dans l'exemple de la thèse (5272111245)
+    occs = occurences(phrase)
+    initial_seq = build_initial_seq(occs)
+    # Fin du Set-up
+
+    # Phase 1
+    comb_tree = combination(initial_seq, debug=debug)
+    if debug:
+        plot_tree(comb_tree, outputname="phase_1_tree")
+
+    # Phase 2
+    leaf_levels = level_assignment(comb_tree, initial_seq)
+    if debug:
+        for leaf, level in leaf_levels:
+            print(leaf.__repr__(0), "niveau", level, "\n")
+
+    # Phase 3
+    # Here, set if you want to use the optimal version of phase 3 or not by uncommenting the right lines
+    #hu_tucker_tree = recombination(leaf_levels, debug=debug)
+    hu_tucker_tree = recombination_stack(leaf_levels, debug=debug)
+    #plot_tree(hu_tucker_tree, label_edges=True, outputname="hu_tucker_tree")
+    plot_tree(hu_tucker_tree, label_edges=True, outputname="hu_tucker_tree_opt")
+
+main(debug=False)
+
